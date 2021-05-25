b9ac288d9cb5658b712377ed1680e5883bd4fd2d
DoDoTeamSandA <dodosolutions4@gmail.com>
2021-05-25 10:22:40 +0530

    first commit

---
+++ app/src/main/java/com/example/pheezee/splash_screen.java
@@ -0,0 +1,29 @@
+package com.example.pheezee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash_screen extends AppCompatActivity {
+    private  static  int SPLASH_SCREEN = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                    startActivity(new Intent(splash_screen.this,Login.class));
+                    finish();
+
+
+
+            }
+        },SPLASH_SCREEN);
+    }
+}
